c1a42732071316b748fffdc2ca0672520ba8f126
Brad Parker <hi@bradparker.com>
2016-01-21 16:04:59 +1000

    Adds client basePath to Header links

--- source/layouts/Header/index.js
+++ source/layouts/Header/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router'
-import config from '../../config'
+import config from '../../../config/environment'
 
 export default (props) => (
   <header>
     <h1>{ props.title }</h1>
     <div>
-      <Link to={ `${ config.basePath }/` }>Index</Link>
-      <Link to={ `${ config.basePath }/about` }>About</Link>
+      <Link to={ `${ config.client.basePath }/` }>Index</Link>
+      <Link to={ `${ config.client.basePath }/about` }>About</Link>
     </div>
   </header>
 )
