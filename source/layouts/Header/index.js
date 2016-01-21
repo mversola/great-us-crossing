84525df8ced89c1a01e375bee1689921ddf51648
Brad Parker <hi@bradparker.com>
2016-01-21 15:56:45 +1000

    Adds base path to header links

--- source/layouts/Header/index.js
+++ source/layouts/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router'
+import config from '../../config'
 
 export default (props) => (
   <header>
     <h1>{ props.title }</h1>
     <div>
-      <Link to="/">Index</Link>
-      <Link to="/about/">About</Link>
+      <Link to={ `${ config.basePath }/` }>Index</Link>
+      <Link to={ `${ config.basePath }/about` }>About</Link>
     </div>
   </header>
 )
