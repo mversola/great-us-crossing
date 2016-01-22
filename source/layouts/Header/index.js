35011d8f5197b03b344a9bc47dc63451bdb8b63e
Brad Parker <hi@bradparker.com>
2016-01-22 14:08:44 +1000

    Menu updates

--- source/layouts/Header/index.js
+++ source/layouts/Header/index.js
@@ -6,9 +6,19 @@ export default (props) => (
   <header>
     <h1>{ props.title }</h1>
     <div>
-      <Link to={ `${ config.client.basePath }/` }>Home</Link>
-      <Link to={ `${ config.client.basePath }/build` }>Building</Link>
-      <Link to={ `${ config.client.basePath }/build/assets` }>Building</Link>
+      <ul>
+        <li>
+          <Link to={ `${ config.client.basePath }/` }>Home</Link>
+        </li>
+        <li>
+          <Link to={ `${ config.client.basePath }/build` }>Building</Link>
+          <ul>
+            <li>
+              <Link to={ `${ config.client.basePath }/build/assets` }>Assets</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </header>
 )
