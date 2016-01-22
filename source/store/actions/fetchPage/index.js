efd0d0f524f0aa25ee35f090fce6f20b2d9317da
Brad Parker <hi@bradparker.com>
2016-01-22 11:29:45 +1000

    Move fetching to action rather than reducer

--- source/store/actions/fetchPage/index.js
+++ source/store/actions/fetchPage/index.js
@@ -1,6 +1,27 @@
+import 'isomorphic-fetch'
+
+import { context } from '../../../config'
+
 import store from '../../'
 
+import receivePageSuccess from '../receivePageSuccess'
+import receivePageFailure from '../receivePageFailure'
+
 export default (route) => {
+  const contentFile = route.replace(/^\/|\/$/g, '')
+
+  fetch(`${ context.host }${context.basePath}/content/${ contentFile }.json`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      receivePageSuccess(route, json)
+    })
+    .catch((err) => {
+      console.log(err)
+      receivePageFailure(route, error)
+    })
+
   store.dispatch({
     type: 'FETCH_PAGE',
     route
