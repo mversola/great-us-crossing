8198eb606102e98a5ba5cb4303786c30f981471b
Brad Parker <hi@bradparker.com>
2016-02-04 16:29:14 +1000

    Adds sass task for compiling vendor sass files (such as from RW and HUI)

--- source/layouts/Document/index.js
+++ source/layouts/Document/index.js
@@ -7,6 +7,7 @@ export default ({ content }) => (
       <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.2/es5-shim.min.js" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.2/es5-sham.min.js" />
+      <link rel="stylesheet" href="/vendor.css" />
       <link rel="stylesheet" href="/main.css" />
     </head>
     <body>
