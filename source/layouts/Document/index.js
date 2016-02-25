bf25df5680b1026640c9987749563f214af01a33
Brad Parker <hi@bradparker.com>
2016-02-26 03:06:44 +1000

    Adds viewport and charset meta

--- source/layouts/Document/index.js
+++ source/layouts/Document/index.js
@@ -5,6 +5,8 @@ export default ({ title, content }) => (
   <html>
     <head>
       <title>{ title }</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.2/es5-shim.min.js" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.2/es5-sham.min.js" />
