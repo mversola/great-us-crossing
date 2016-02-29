72de406f88d23de741c33a1159e8c32837867023
Mysamaris <admin@mysamaris.com>
2016-02-29 13:29:41 -0600

    Update index.js

--- source/components/NavLink/index.js
+++ source/components/NavLink/index.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router'
 import React from 'react'
 import config from '../../../config/environment'
+import styles from './styles.css'
 
 const { client: { basePath }} = config
 
 export default (props) => (
+  <center><img className={ styles.image } src={ props.image } /></center>
   <Link
     { ...props }
     to={ `${ basePath }${ props.to }` }
