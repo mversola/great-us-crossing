74319270aabe297b7c13a65b52a86bc2863300b7
Brad Parker <hi@bradparker.com>
2016-03-02 17:51:56 +1000

    Replace backslash and forward slash in paths

--- build/css-modules-scope-generator.js
+++ build/css-modules-scope-generator.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
 module.exports = (local, filename) => {
-  filename = path.dirname(filename.replace(/.+?source/, '')).replace(/\//g, '_')
+  filename = path.dirname(filename.replace(/.+?source/, '')).replace(/\/|\\/g, '_')
   return `${filename}__${ local }`
 }
