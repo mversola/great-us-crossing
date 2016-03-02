47d3fa6f6b58d90c3afc9619ee429fbc4ec31b4d
Brad Parker <hi@bradparker.com>
2016-03-02 18:08:43 +1000

    [WIP] log path from name generator

--- build/css-modules-scope-generator.js
+++ build/css-modules-scope-generator.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = (local, filename) => {
+  console.log(filename)
   filename = path.dirname(filename.replace(/.+?source/, '')).replace(/\/|\\/g, '_')
   return `${filename}__${ local }`
 }
