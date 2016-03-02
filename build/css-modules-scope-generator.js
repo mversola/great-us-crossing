b58063a6b9f90ac7113872c3b0fbb2dba72409b3
Brad Parker <hi@bradparker.com>
2016-03-02 18:35:19 +1000

    Revert "[WIP] log path from name generator"
    This reverts commit 47d3fa6f6b58d90c3afc9619ee429fbc4ec31b4d.

--- build/css-modules-scope-generator.js
+++ build/css-modules-scope-generator.js
@@ -1,7 +1,6 @@
 const path = require('path')
 
 module.exports = (local, filename) => {
-  console.log(filename)
   filename = path.dirname(filename.replace(/.+?source/, '')).replace(/\/|\\/g, '_')
   return `${filename}__${ local }`
 }
