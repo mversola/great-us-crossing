f0421d5f798615eafbabe317892882703546d65a
Brad Parker <hi@bradparker.com>
2016-03-12 06:43:11 +1000

    Footer call to action and darker blues

--- source/layouts/Footer/index.js
+++ source/layouts/Footer/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import Container from '../../components/Container'
 import TextContent from '../../components/TextContent'
+import CallToActionLink from '../../components/CallToActionLink'
 import styles from './styles.css'
 import config from '../../../config/environment'
 
-const { client: { basePath }} = config
+const { client: { host, basePath }} = config
 
 export default () => (
   <footer className={ styles.base }>
     <Container>
        <TextContent theme="alignCenter">
-         <p><em>Fundraising powered by</em><img src="/layouts/Footer/everydayhero.png" /></p>
-         <p>
-           <a href="https://everydayhero.com/us/terms/privacy">PRIVACY POLICY</a>
-           <a href={ `${basePath}/terms_and_conditions.pdf` }>TERMS &amp; CONDITIONS</a>
-           <a href="https://supporter.help-us.everydayhero.com/hc/en-us/sections/201814296-Great-US-Crossing">HELP</a>
+        <p>
+          <CallToActionLink href="https://greatuscrossing.everydayhero.com/us/get-started" theme="outlineNegative">
+            Register
+          </CallToActionLink>
+        </p>
+        <p>
+          <em>Fundraising powered by</em><br/>
+          <img src="/layouts/Footer/everydayhero.png" />
          </p>
-       </TextContent>
+        <p>
+          <a href="https://everydayhero.com/us/terms/privacy">PRIVACY POLICY</a>
+          <a href={ `${host}${basePath}/terms_and_conditions.pdf` }>TERMS &amp; CONDITIONS</a>
+          <a href="https://supporter.help-us.everydayhero.com/hc/en-us/sections/201814296-Great-US-Crossing">HELP</a>
+        </p>
+      </TextContent>
     </Container>
   </footer>
 )
