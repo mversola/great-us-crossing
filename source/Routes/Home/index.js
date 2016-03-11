a9a3ca920157e7ee903c130ba1f7c76a21fce72d
Brad Parker <hi@bradparker.com>
2016-03-12 01:04:46 +1000

    Uses Icon font for icon page nav

--- source/Routes/Home/index.js
+++ source/Routes/Home/index.js
@@ -7,6 +7,7 @@ import Divider from '../../components/Divider'
 import Grid from '../../components/Grid'
 import GridItem from '../../components/Grid/Item'
 import Hero from '../../components/Hero'
+import Icon from '../../components/Icon'
 import Logo from '../../components/Logo'
 import Main from '../../layouts/Main'
 import NavLink from '../../components/NavLink'
@@ -50,26 +51,42 @@ export default () => (
         <Grid spacing="none">
           <GridItem divisions={['one-quarter']}>
             <NavLink to="/#about" className={ styles.PageNav__NavLink }>
-              <img className={ styles.PageNav__NavImage } src="/Routes/Home/greatuscrossing_bike.png" />
-              About
+              <div className={ styles.PageNav__NavLink__icon }>
+                <Icon icon="bike" />
+              </div>
+              <span className={ styles.PageNav__NavLink__text }>
+                About
+              </span>
             </NavLink>
           </GridItem>
           <GridItem divisions={['one-quarter']}>
             <NavLink to="/#route" className={ styles.PageNav__NavLink }>
-              <img className={ styles.PageNav__NavImage } src="/Routes/Home/greatuscrossing_pin.png" />
-              Route
+              <div className={ styles.PageNav__NavLink__icon }>
+                <Icon icon="map" />
+              </div>
+              <span className={ styles.PageNav__NavLink__text }>
+                Route
+              </span>
             </NavLink>
           </GridItem>
           <GridItem divisions={['one-quarter']}>
             <NavLink to="/#prizes" className={ styles.PageNav__NavLink }>
-              <img className={ styles.PageNav__NavImage } src="/Routes/Home/greatuscrossing_trophy.png" />
-              Prizes
+              <div className={ styles.PageNav__NavLink__icon }>
+                <Icon icon="prize" />
+              </div>
+              <span className={ styles.PageNav__NavLink__text }>
+                Prizes
+              </span>
             </NavLink>
           </GridItem>
           <GridItem divisions={['one-quarter']}>
             <NavLink to="/#fundraising" className={ styles.PageNav__NavLink }>
-              <img className={ styles.PageNav__NavImage } src="/Routes/Home/greatuscrossing_hands.png" />
-              Fundrasing
+              <div className={ styles.PageNav__NavLink__icon }>
+                <Icon icon="heart" />
+              </div>
+              <span className={ styles.PageNav__NavLink__text }>
+                Fundrasing
+              </span>
             </NavLink>
           </GridItem>
         </Grid>
