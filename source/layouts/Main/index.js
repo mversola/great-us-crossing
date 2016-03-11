52a486131ba9a689dce5471db84a3151f7f355cb
Brad Parker <hi@bradparker.com>
2016-03-12 07:47:10 +1000

    Uses Document title in Main layout

--- source/layouts/Main/index.js
+++ source/layouts/Main/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { RouteHandler } from 'react-router'
+import DocumentTitle from 'react-document-title'
 import Header from '../Header'
 import Footer from '../Footer'
 
 import styles from './styles.css'
 
 export default (props) => (
-  <article className={ styles.Main }>
-    { props.children }
-    <Footer { ...props } />
-  </article>
+  <DocumentTitle title={ props.title }>
+    <article className={ styles.Main }>
+      { props.children }
+      <Footer { ...props } />
+    </article>
+  </DocumentTitle>
 )
 
