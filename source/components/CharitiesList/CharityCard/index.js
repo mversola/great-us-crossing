915f7739fc4c6112100ee9dc0c22f258947b7c53
Brad Parker <hi@bradparker.com>
2016-03-15 06:50:46 +1000

    With four charities to start, split into quarters

--- source/components/CharitiesList/CharityCard/index.js
+++ source/components/CharitiesList/CharityCard/index.js
@@ -3,7 +3,7 @@ import GridItem from '../../../components/Grid/Item'
 import styles from './styles.css'
 
 export default ({ image, href }) => (
-  <GridItem divisions={['one-half', 'medium-one-quarter', 'regular-one-fifth']}>
+  <GridItem divisions={['one-half', 'medium-one-quarter']}>
     <a href={ href }>
       <div className={ styles.base }>
         <img className={ styles.image } src={image} />
