da3c59bba846d0522e854fc45b75136bf595efd8
Brad Parker <hi@bradparker.com>
2016-03-21 22:00:22 +1000

    Don't try to access L object without a DOM, at least while it's
    being loaded from a script tag

--- source/components/TourTracker/index.js
+++ source/components/TourTracker/index.js
@@ -12,28 +12,30 @@ export default class extends React.Component {
   }
 
   componentDidMount () {
-    this._L = global.L
-    this._L.mapbox.accessToken = 'pk.eyJ1IjoicGFya2VyYnJhZHMiLCJhIjoiY2lsYWVua2lhMDR4aXVna250aXc3eG54OCJ9.QjhkzoGgIOoLpV9k-ImrBw'
-    this._map = this._L.map(ReactDOM.findDOMNode(this), { zoomControl: false })
+    if (global.L) {
+      this._L = global.L
+      this._L.mapbox.accessToken = 'pk.eyJ1IjoicGFya2VyYnJhZHMiLCJhIjoiY2lsYWVua2lhMDR4aXVna250aXc3eG54OCJ9.QjhkzoGgIOoLpV9k-ImrBw'
+      this._map = this._L.map(ReactDOM.findDOMNode(this), { zoomControl: false })
 
-    this._map.dragging.disable()
-    this._map.touchZoom.disable()
-    this._map.doubleClickZoom.disable()
-    this._map.scrollWheelZoom.disable()
-    this._map.keyboard.disable()
+      this._map.dragging.disable()
+      this._map.touchZoom.disable()
+      this._map.doubleClickZoom.disable()
+      this._map.scrollWheelZoom.disable()
+      this._map.keyboard.disable()
 
-    if (this._map.tap) this._map.tap.disable()
+      if (this._map.tap) this._map.tap.disable()
 
-    const routePoints = this.props.route.map(
-      (routeDatum) => routeDatum.point
-    )
-    this._map.fitBounds(routePoints, {
-      padding: [50, 50]
-    })
-    this._L.tileLayer('https://api.mapbox.com/v4/parkerbrads.6bc7d29e/{z}/{x}/{y}@2x.png?access_token=' + this._L.mapbox.accessToken, {
-      attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this._map)
-    this.renderRoutes()
+      const routePoints = this.props.route.map(
+        (routeDatum) => routeDatum.point
+      )
+      this._map.fitBounds(routePoints, {
+        padding: [50, 50]
+      })
+      this._L.tileLayer('https://api.mapbox.com/v4/parkerbrads.6bc7d29e/{z}/{x}/{y}@2x.png?access_token=' + this._L.mapbox.accessToken, {
+        attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(this._map)
+      this.renderRoutes()
+    }
   }
 
   shouldComponentUpdate () {
